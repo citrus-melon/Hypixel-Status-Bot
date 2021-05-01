3772deecd3dea81c658e2524920d06d4a99cbad7
citrus-melon <58640629+citrus-melon@users.noreply.github.com>
2021-05-01 09:20:59 -0400

    Add friendly duration helper function

---
+++ src/helpers/friendlyDuration.js
@@ -0,0 +1,7 @@
+/** @param {number} minutes */
+module.exports = (totalMinutes) => {
+    const hours = Math.floor(totalMinutes / 60);
+    const minutes = totalMinutes % 60;
+    if (hours) return `${hours} ${hours===1 ? 'hour' : 'hours'} and ${minutes} ${minutes===1 ? 'minute' : 'minutes'}`;
+    else return `${minutes} ${minutes===1 ? 'minute' : 'minutes'}`;
+}
