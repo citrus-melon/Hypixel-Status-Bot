b2d3275ac88bc3aac072952941ded95384d85fea
citrus-melon <58640629+citrus-melon@users.noreply.github.com>
2021-05-02 20:39:18 -0400

    Fix month creation code creating days instead

--- src/helpers/playerHelpers.js
+++ src/helpers/playerHelpers.js
@@ -26,8 +26,8 @@ const tryChangeDays = (player, now) => {
     if(!player.dailyHistory[player.dailyHistory.length-1]) player.dailyHistory[player.dailyHistory.length-1] = 0;
 
     const monthDelta = now.getMonth() - new Date(player.lastIncremented).getMonth();
-    for (let index = 0; index < dayDelta; index++) {
-        player.dailyHistory.push(null);
+    for (let index = 0; index < monthDelta; index++) {
+        player.monthlyHistory.push(null);
     }
     if(!player.monthlyHistory[player.monthlyHistory.length-1]) player.monthlyHistory[player.monthlyHistory.length-1] = 0;
 
