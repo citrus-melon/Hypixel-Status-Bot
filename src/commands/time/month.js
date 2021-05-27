add065f1157c3fd6f4ee73c196bf95a3a6f95d70
citrus-melon <58640629+citrus-melon@users.noreply.github.com>
2021-05-26 21:16:06 -0400

    Add months ago functionality to month command

--- src/commands/time/month.js
+++ src/commands/time/month.js
@@ -1,17 +1,18 @@
 const { MessageEmbed } = require('discord.js');
 const { Command } = require('discord.js-commando');
+const agoString = require('../../helpers/agoString');
+const friendlyDuration = require('../../helpers/friendlyDuration');
 const playerHelpers = require('../../helpers/playerHelpers');
 const usernameCache = require('../../usernameCache');
-const friendlyDuration = require('../../helpers/friendlyDuration');
 
-module.exports = class thisMonthPlaytime extends Command {
+module.exports = class monthPlaytime extends Command {
     constructor(client) {
         super(client, {
-            name: 'monthtotal',
+            name: 'month',
             group: 'time',
             memberName: 'month',
-            description: 'Get a player\'s total tracked playtime of a player this month',
-            examples: ['monthtotal', 'monthtotal citrus_melon', 'monthtotal @citrus-melon'],
+            description: 'Get a player\'s total tracked playtime during a certain month. Defaults to the current month.',
+            examples: ['month', 'month citrus_melon', 'month @citrus-melon'],
             throttling: {duration: 10, usages: 5},
             args: [
                 {
@@ -20,27 +21,37 @@ module.exports = class thisMonthPlaytime extends Command {
                     prompt: 'Whoose stats would you like to get? Enter a Minecraft username or mention a Discord user',
                     type: 'mention|minecraftaccount',
                     default: message => message.author
+                },
+                {
+                    key: 'monthsAgo',
+                    type: 'integer',
+                    label: 'months ago',
+                    prompt: 'How many months ago is the month you would like to get? (Ex. last month = 1)',
+                    min: 0,
+                    default: 0
                 }
             ]
         });
     }
 
     /** @param {import('discord.js-commando').CommandoMessage} message */
-    async run(message, { account }) {
+    async run(message, { account, monthsAgo }) {
         const player = await playerHelpers.getDiscordOrMinecraft(account, {monthlyHistory: 1, lastIncremented: 1});
         if (typeof player === 'string') { message.reply(player); return; }
 
         const adjustedHistory = playerHelpers.adjustMonthlyHistory(player.monthlyHistory, player.lastIncremented, new Date());
-        const value = adjustedHistory[0];
+        const value = adjustedHistory[monthsAgo];
         const username = await usernameCache.getUsernameByID(player._id);
         const embed = new MessageEmbed();
 
-        embed.setDescription(`${username} has played for **${friendlyDuration(value)}** in total this month!`);
-
         embed.setAuthor(username, `https://crafatar.com/avatars/${player._id}`, `https://namemc.com/profile/${player._id}`);
-        embed.setTitle(`${username}'s total playtime by weekday`);
+        embed.setTitle(`${username}'s total playtime ${agoString.months(monthsAgo)}`);
         embed.setFooter('(Only while tracked)');
         embed.setTimestamp(player.lastIncremented);
+
+        if (value === null || value === undefined) embed.setDescription(`${username} wasn't tracked ${agoString.months(monthsAgo)}!`);
+        else embed.setDescription(`${username} played **${friendlyDuration(value)}** in total ${agoString.months(monthsAgo)}!`);
+
         message.reply(embed);
     }
 };
